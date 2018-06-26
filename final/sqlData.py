26f07cc5f962d306291849c78f27a1d2719a5e15
ywdxy123 <619998073@qq.com>
2018-06-26 14:06:47 +0800

    Update sqlData.py

--- final/sqlData.py
+++ final/sqlData.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 import pandas as pd
-conn = create_engine('mysql+pymysql://root:hantele@192.168.6.10:5029/test10086?charset=utf8',pool_size=10000)
-#conn = create_engine('mysql+pymysql://root:hantele@192.168.6.10:5029/Data_0104?charset=utf8')
+conn = create_engine('mysql+pymysql://root:xxxxxxxxxxxxxxxxxxxxxxxxxxxcharset=utf8',pool_size=10000)
 def getData(sql_str):
 	data=pd.read_sql(sql_str,conn)
 	return data
